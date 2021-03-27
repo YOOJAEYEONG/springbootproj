d796b30643bfeb4e850c455f6ba31f4065c538d4
liujeayat <liujeayat@gmail.com>
2021-03-28 00:31:18 +0900

    Proxy(default) Transaction 추가

---
+++ src/main/java/com/practice/springbootproj/common/transactionconfig/AnnotationTransactionConfig.java
@@ -0,0 +1,30 @@
+package com.practice.springbootproj.common.transactionconfig;
+
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.datasource.DataSourceTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+
+
+/**
+ * <pre>
+ *   어노테이션기반 Transaction 설정을 위한 Configuration
+ * </pre>
+ */
+
+@RequiredArgsConstructor
+@EnableTransactionManagement
+public class AnnotationTransactionConfig {
+  //@EnableTransactionManagement 어노테이션을 등록하면 @Transactional 로 설정이 가능하다.
+
+  private final DataSource dataSource;
+
+  @Bean(name = "txManager")
+  public PlatformTransactionManager txManager(){
+    return new DataSourceTransactionManager(dataSource);
+  }
+}
